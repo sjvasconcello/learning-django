e9e4e6e25063a946be16c3c6c4c9716264873051
sjvasconcello <sjvasconcello@gmail.com>
2020-06-30 00:06:44 -0400

    :construction: Working in reset password
    Change your password and password changed added - Django 3 by Example pag. 110

--- social_website/bookmarks/account/urls.py
+++ social_website/bookmarks/account/urls.py
@@ -5,5 +5,11 @@ from . import views
 urlpatterns = [
     path("", views.dashboard, name="dashboard"),
     path('login/', auth_views.LoginView.as_view(), name='login'),
-   path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    # Change password urls
+    path("password_change/", auth_views.PasswordChangeView.as_view(),
+         name="password_change"),
+    path("password_change/done/", auth_views.PasswordChangeDoneView.as_view(),
+         name="password_change_done")
+
 ]
